5a7757e5dcdce7632a706b0834bc93cd48a32a5a
matute <tosomatias@gmail.com>
2021-03-08 11:45:51 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="infinit">
+      <div className="objet"></div>
+      <div className="shadow"></div>
+    </div>
+  );
+}
+
+export default App;
